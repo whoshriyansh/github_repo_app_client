e00feb20c1d8b3c948bb776ee3797f78b4b22cb8
whoshriyansh <shriyanshlohia0@gmail.com>
2024-09-03 09:55:51 +0530

    UI Done without any Functionality

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Search from "../components/Search";
+import ProfileInfo from "../components/ProfileInfo";
+import Repos from "../components/Repos";
+import Spinner from "../components/Spinner";
+import SortRepos from "../components/SortRepos";
+
+const HomePage = () => {
+  return (
+    <div className="m-4">
+      <Search />
+      <SortRepos />
+      <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
+        <ProfileInfo />
+        <Repos />
+        <Spinner />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
